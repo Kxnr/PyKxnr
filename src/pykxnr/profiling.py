c65df96e774dfd8fb44a7d9cbf4de6c690b2fb82
Connor Keane <ckeane@ascendanalytics.com>
2022-05-25 15:23:19 -0600

    add basic profiling utilities

--- src/pykxnr/profiling.py
+++ src/pykxnr/profiling.py
@@ -3,25 +3,30 @@ import tracemalloc
 import os
 import datetime
 from functools import wraps
+import linecache
+
+def take_snapshot(prefix=None, output_dir="."):
+    snapshot = tracemalloc.take_snapshot()
+
+    abspath = os.path.abspath(output_dir)
+    os.makedirs(abspath, exist_ok=True)
+    
+    snapshot_file = os.path.join(abspath, f"{prefix or 'snapshot'}_{datetime.datetime.now().isoformat()}.snp")
+    print(f"Saving memory usage snapshot to: {snapshot_file}")
+    snapshot.dump(snapshot_file)
 
 
 def profile(nframe=1, output_dir="."):
-    abspath = os.path.abspath(output_dir)
-    os.makedirs(abspath, exist_ok=True)
-
     def wrapper(func):
         @wraps(func)
         def _wrapped(*args, **kwargs):
             tracemalloc.start(nframe)
+            # tracemalloc.reset_peak()
             value = func(*args, **kwargs)
-            snapshot = tracemalloc.take_snapshot()
-            
-            snapshot_file = os.path.join(abspath, f"{func.__name__}_{datetime.datetime.now().isoformat()}.pkl")
-            print(f"Saving memory usage snapshot to: {snapshot_file}")
+            take_snapshot(prefix=func.__name__, output_dir=output_dir)
+            _, peak = tracemalloc.get_traced_memory()
 
-            with open(snapshot_file, 'wb') as f:
-                pickle.dump(snapshot, f)
-
+            print(f"Peak usage: {bytes_to_gibibytes(peak):.4f} GiB")
             tracemalloc.stop()
 
             return value
@@ -30,7 +35,29 @@ def profile(nframe=1, output_dir="."):
 
 
 def load_profile(path):
-    with open(path, 'rb') as f:
-        return pickle.load(f)
+    return tracemalloc.Snapshot.load(path)
+
+def bytes_to_gibibytes(size: int):
+    return size / (1024 * 1024 * 1024)
 
 
+
+def summarize_snapshot(snapshot, limit=20):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap_external>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+
+    by_line = snapshot.statistics("lineno")
+
+    total = sum([line.size for line in by_line])
+
+    print(f"Total traced memory usage {bytes_to_gibibytes(total):.4f} GiB")
+    print(f"Top {limit} largest allocations.")
+    for ind, stat in enumerate(by_line[:limit], 1):
+        frame, *_ = stat.traceback
+        print(f"#{ind}: {frame.filename}:{frame.lineno} {bytes_to_gibibytes(stat.size):.4f} GiB")
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+
+    print(f"\t{line}")  
